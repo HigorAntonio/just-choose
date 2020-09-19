8aeaf006ac91b601edcb9fa83f54d9994f5fba84
Higor Antonio <higorantonio@pop-os.localdomain>
2020-09-18 23:56:02 -0300

    Conexão com o banco de dados

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,44 +1,15 @@
 // Update with your config settings.
+require('dotenv/config');
+const path = require('path');
 
 module.exports = {
-
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+  client: 'pg',
+  connection: {
+    database: "skip_movie",
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  migrations: {
+    directory: path.resolve(__dirname, 'src', 'database', 'migrations')
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
---
+++ backend/src/crawler.js
@@ -0,0 +1,3 @@
+const knex = require('./database');
+
+console.log('ok');
