0092f860595b7b847b427655c34a842153a452d3
Higor Antonio <higorantonio@pop-os.localdomain>
2020-09-19 01:21:25 -0300

    Buscando providers na web e inserindo-os no banco de dados

--- backend/src/crawler.js
+++ backend/src/crawler.js
@@ -1,3 +1,27 @@
+const axios = require('axios');
 const knex = require('./database');
 
-console.log('ok');
+(async () => {
+  const url = 'https://apis.justwatch.com/content/providers/locale/pt_BR';
+
+  try {
+    const response = await axios.get(url);
+    // console.log(response.data);
+
+    const providers = response.data.map(({ clear_name, short_name }) => (
+      { name: clear_name, short_name }
+    ));
+    // console.log(providers);
+    
+    for (provider of providers) {
+      await knex('providers').insert(provider);
+      console.log(provider);
+    }
+    console.log('Banco populado')
+    
+  } catch (err) {
+    console.log('Something went wrong ' + err);
+  }
+
+  knex.destroy();
+})();
--- backend/src/database/migrations/20200918235715_create_table_providers.js
+++ backend/src/database/migrations/20200918235715_create_table_providers.js
@@ -2,7 +2,7 @@ exports.up = async knex => knex.schema.createTable('providers', table => {
   table.increments('id');
   table.text('name').unique().notNullable();
   table.text('short_name').unique().notNullable();
-  table.text('icon_url');
+  table.text('icon_url').defaultTo(null);
 
   table.timestamp('created_at').defaultTo(knex.fn.now());
   table.timestamp('updated_at').defaultTo(knex.fn.now());
