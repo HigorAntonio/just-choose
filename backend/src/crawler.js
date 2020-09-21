1273a68b502a37651dc176e686fb3c68d25f0dd4
Higor Antonio <higorantonio@pop-os.localdomain>
2020-09-21 11:54:32 -0300

    Obtendo dados dos filmes

--- backend/src/crawler.js
+++ backend/src/crawler.js
@@ -8,13 +8,13 @@ async function getData({ baseURL, url, params = {} }) {
   } catch (err) {
     if (err.response) {
       console.log(`Erro: Request: { baseURL: ${baseURL}, url: ${url}, ` + 
-      `params: ${params} }. Código de status http ${err.response.status}`);
+      `params: ${JSON.stringify(params)} }. Código de status http ${err.response.status}`);
     } else if (err.request) {
       console.log(`Erro: Request: { baseURL: ${baseURL}, url: ${url}, ` + 
-      `params: ${params} }. Sem resposta do servidor`);
+      `params: ${JSON.stringify(params)} }. Sem resposta do servidor`);
     } else {
       console.log(`Erro: Request: { baseURL: ${baseURL}, url: ${url},` + 
-      ` params: ${params} }. ${err.message}`);
+      ` params: ${JSON.stringify(params)} }. ${err.message}`);
     }
     throw err;
   }
@@ -45,11 +45,12 @@ function sanitizeGenres(genresData) {
 async function getAllGenresUrl() {
   try {
     const genres = await knex('genres').select('short_name');
-    let genresUrl = '';
-    for (genre of genres) {
-      genresUrl = genresUrl.concat(`"${genre.short_name}", `);
-    }
-    return genresUrl.slice(0, -2);
+    // let genresUrl = '';
+    // for (genre of genres) {
+    //   genresUrl = genresUrl.concat(`"${genre.short_name}", `);
+    // }
+    // return genresUrl.slice(0, -2);
+    return genres.map(genre => `${genre.short_name}`);
   } catch (err) {
     console.log(`Erro: getAllGenresUrl(). ${err.message}`);
     throw err;
@@ -60,6 +61,24 @@ async function getAllGenresUrl() {
   const baseURL = 'https://apis.justwatch.com/';
   const providersUrl = '/content/providers/locale/pt_BR';
   const genresUrl = '/content/genres/locale/pt_BR';
+  const moviesUrl =  '/content/titles/pt_BR/popular';
+  const moviesParams = { 
+    body: {
+      fields: [
+        "cinema_release_date", "full_path", "full_paths" , 
+        "id", "localized_release_date", "object_type", "poster", 
+        "scoring", "title", "tmdb_popularity", "offers"
+      ],
+      content_types: ["movie"],
+      genres: await getAllGenresUrl(),
+      providers:["prv", "nfx"],
+      enable_provider_filter: false,
+      monetization_types:[],
+      page: 1,
+      page_size:30,
+      matching_offers_only:true
+    }
+  }
 
   try {
     const providersData = await getData({ baseURL, url: providersUrl });
@@ -88,6 +107,10 @@ async function getAllGenresUrl() {
       console.log(genre);
     }
     console.log('Banco populado com genres')
+    
+    
+    const moviesData = await getData({ baseURL, url: moviesUrl, params: moviesParams });
+    // console.log(moviesData);
 
   } catch (err) {
     console.log('Something went wrong ' + err);
@@ -96,4 +119,4 @@ async function getAllGenresUrl() {
   knex.destroy();
 })();
 
-'https://apis.justwatch.com/content/titles/pt_BR/popular?body=%7B"fields":["cinema_release_date","full_path","full_paths","id","localized_release_date","object_type","poster","scoring","title","tmdb_popularity","offers"],"content_types":["movie"],"genres":["msc","act","ani","cmy","crm","fml","eur","hrr","hst","fnt","drm","doc","rly","wsn","war","scf","rma","trl","spt"],"providers":["prv","nfx"],"enable_provider_filter":false,"monetization_types":[],"page":2,"page_size":30,"matching_offers_only":true%7D'
+// 'https://apis.justwatch.com/content/titles/pt_BR/popular?body=%7B"fields":["cinema_release_date","full_path","full_paths","id","localized_release_date","object_type","poster","scoring","title","tmdb_popularity","offers"],"content_types":["movie"],"genres":["msc","act","ani","cmy","crm","fml","eur","hrr","hst","fnt","drm","doc","rly","wsn","war","scf","rma","trl","spt"],"providers":["prv","nfx"],"enable_provider_filter":false,"monetization_types":[],"page":2,"page_size":30,"matching_offers_only":true%7D'
