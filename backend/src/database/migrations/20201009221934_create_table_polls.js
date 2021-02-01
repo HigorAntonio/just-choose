2a11581eb7a546cf25ebdcf237e90a5cc0c87aae
HigorAntonio <higorantonio.ha@gmail.com>
2021-01-31 23:25:55 -0300

    Adiçao da coluna tumbnail na tabela de votaçoes

--- backend/src/database/migrations/20201009221934_create_table_polls.js
+++ backend/src/database/migrations/20201009221934_create_table_polls.js
@@ -1,17 +1,22 @@
 const { onUpdateTrigger } = require('../triggers');
 
-exports.up = async knex => knex.schema.createTable('polls', table => {
-  table.increments('id').primary();
-  table.integer('user_id')
-    .references('users.id')
-    .notNullable()
-    .onUpdate('CASCADE')
-    .onDelete('CASCADE');
-  table.text('title').notNullable();
+exports.up = async (knex) =>
+  knex.schema
+    .createTable('polls', (table) => {
+      table.increments('id').primary();
+      table
+        .integer('user_id')
+        .references('users.id')
+        .notNullable()
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      table.text('title').notNullable();
+      table.text('thumbnail');
 
-  table.timestamp('created_at').defaultTo(knex.fn.now());
-  table.timestamp('updated_at').defaultTo(knex.fn.now());
-  table.timestamp('deleted_at');
-}).then(() => knex.raw(onUpdateTrigger('polls')));
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.timestamp('updated_at').defaultTo(knex.fn.now());
+      table.timestamp('deleted_at');
+    })
+    .then(() => knex.raw(onUpdateTrigger('polls')));
 
-exports.down = async knex => knex.schema.dropTable('polls');
+exports.down = async (knex) => knex.schema.dropTable('polls');
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,14 @@
 require('dotenv/config')
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 const updateMovieData = require('./schedule/updateMovieDataSchedule');
 
 const app = express();
 
 updateMovieData();
+
+app.use(cors());
 
 app.use(express.json());
 
