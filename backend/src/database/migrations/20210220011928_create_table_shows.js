3a31289a4c2d4fc27cb2efb611686ad83479f03c
Higor Antonio <higorantonio.ha@gmail.com>
2021-04-17 15:57:45 -0300

    Permite valor null para imagem de capa de filmes, series e jogos

--- backend/src/database/migrations/20210220011928_create_table_shows.js
+++ backend/src/database/migrations/20210220011928_create_table_shows.js
@@ -7,7 +7,7 @@ exports.up = async (knex) =>
       table.integer('tmdb_id').unique().notNullable();
       table.text('original_name').notNullable();
       table.text('name').notNullable();
-      table.text('poster_path').notNullable();
+      table.text('poster_path');
 
       table.timestamp('created_at').defaultTo(knex.fn.now());
       table.timestamp('updated_at').defaultTo(knex.fn.now());
--- backend/src/database/migrations/20210220012048_create_table_games.js
+++ backend/src/database/migrations/20210220012048_create_table_games.js
@@ -6,7 +6,7 @@ exports.up = async (knex) =>
       table.increments('id').primary();
       table.integer('rawg_id').unique().notNullable();
       table.text('name').notNullable();
-      table.text('background_image').notNullable();
+      table.text('background_image');
 
       table.timestamp('created_at').defaultTo(knex.fn.now());
       table.timestamp('updated_at').defaultTo(knex.fn.now());
